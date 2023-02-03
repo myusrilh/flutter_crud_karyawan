6f9a99b2438cc63a350fb53362f888bfa05a56ce
yusrilhasriansyah@gmail.com <yusrilhasriansyah@gmail.com>
2023-02-03 14:18:06 +0700

    update custom data comment

--- lib/helper/custom_data.dart
+++ lib/helper/custom_data.dart
@@ -5,7 +5,7 @@ var lightGreenColorTheme = const Color(0xFFACD4A5);
 var whiteColorTheme = const Color(0xFFF7F7F7);
 var createDataButton = const Color(0xFF0096FF);
 
-String urlBase = "http://192.168.100.56:3000/api";
-
+String urlBase =
+    "http://192.168.100.56:3000/api"; // untuk hp fisik (pakai IP dari server/laptop yang menjalankan)
   // String urlBase = "http://10.0.2.2:3000/api"; // untuk AVD/Emulator atau bisa diganti 127.0.0.1:3000
-  // String urlBase = "http://192.168.100.56:3000/api"; // untuk hp fisik (pakai IP dari server/laptop yang menjalankan)
+  // String urlBase = "http://localhost:port/api"; //default
